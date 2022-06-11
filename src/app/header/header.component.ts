507cc0f7c8f958ab595010d5917128d203ed176f
Jeth0214 <rolandjethrosuyom@yahoo.com>
2022-06-11 20:38:52 +0300

    used css custom properties instead of a static value for theming

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { DarkModeService } from 'angular-dark-mode';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-  isDarkMode: boolean;
+export class HeaderComponent {
 
-  constructor(private darkModeService: DarkModeService) { }
 
-  ngOnInit(): void {
-    this.darkModeService.darkMode$.subscribe(theme => this.isDarkMode = theme)
-  }
+
+
 }

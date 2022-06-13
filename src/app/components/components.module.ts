4823bf32786d8ca7becfedfc9971f7417b39a49b
Jeth0214 <rolandjethrosuyom@yahoo.com>
2022-06-13 20:06:28 +0300

    finished styling serach component and start on dropdown component

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DropdownComponent } from './dropdown/dropdown.component';
+
+
+
+@NgModule({
+  declarations: [
+    DropdownComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    DropdownComponent
+  ]
+})
+export class ComponentsModule { }

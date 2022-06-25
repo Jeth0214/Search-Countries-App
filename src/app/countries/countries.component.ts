d9eb19d6d7ff918b0eccfd4b3bb959c62bf07c0e
Jeth0214 <rolandjethrosuyom@yahoo.com>
2022-06-25 04:28:12 +0300

    added ngx-spinner

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Country } from '../models/country';
 import { CountriesService } from '../countries.service';
 import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-countries',
@@ -10,16 +11,22 @@ import { Router } from '@angular/router';
 export class CountriesComponent implements OnInit {
 
   countries: Country[];
+  showSpinner = false;
 
-  constructor(private countriesService: CountriesService, private router: Router) { }
+  constructor(private countriesService: CountriesService, private router: Router, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
     this.getCountries();
   }
 
   getCountries() {
+    this.showSpinner = true;
+    this.spinner.show();
     this.countriesService.getAllCountries().subscribe(data => {
+      document.body.scrollTop = document.documentElement.scrollTop = 0;
       this.countries = data;
+      this.spinner.hide();
+      this.showSpinner = false;
     })
   }
 
@@ -28,8 +35,12 @@ export class CountriesComponent implements OnInit {
   }
 
   filterByRegion(region) {
+    this.showSpinner = true;
+    this.spinner.show();
     this.countriesService.getCountriesByRegion(region).subscribe(countries => {
-      this.countries = countries
+      this.countries = countries;
+      this.spinner.hide();
+      this.showSpinner = false;
     },
     );
   }
--- src/app/search-filter/search-filter.component.ts
+++ src/app/search-filter/search-filter.component.ts
@@ -1,8 +1,4 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { catchError, debounceTime, distinctUntilChanged, filter, map, Observable, retry, startWith, Subject, switchMap, tap } from 'rxjs';
-import { CountriesService } from '../countries.service';
-import { Country } from '../models/country';
 
 const REGIONS = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
 
@@ -14,39 +10,14 @@ export class SearchFilterComponent implements OnInit {
   @Output() filter = new EventEmitter<string>();
 
   regions: string[] = REGIONS;
-  foundCountry: boolean = false;
-  countries$!: Observable<Country[]>;
-  searchBox: FormControl = new FormControl('');
-  showList: boolean = false;
-  results;
-
-  private searcTerms = new Subject<string>();
-
-  constructor(private countryService: CountriesService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.countries$ = this.searchBox.valueChanges.pipe(
-      map(search => search.trim()),
-      debounceTime(200),
-      distinctUntilChanged(),
-      switchMap((term: string) => this.countryService.getCountryByName(term).pipe(
-        retry(3),
-        startWith([])
-      )),
 
-    )
-    this.countries$.subscribe(results => {
-      this.results = results
-      this.showList = results.length > 0 ? true : false;
-    }
-    )
   }
 
   onFilter(region) {
     this.filter.emit(region);
   }
 
-  onSearch(term: string): void {
-    this.searcTerms.next(term.trim());
-  }
 }
